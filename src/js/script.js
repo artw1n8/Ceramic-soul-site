f0f62bf09da7073f3e9c9f0b7903823653ea8fb2
art tem <kalinichenkoartemiywork@gmail.com>
2025-08-09 12:13:34 +0300

    created slider and footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,38 @@
-import "purecss/build/grids-min.css"
-import "purecss/build/grids-responsive-min.css"
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import "purecss/build/grids-min.css";
+import "purecss/build/grids-responsive-min.css";
 import "/src/sass/style.scss";
+
+try {
+    const swiper = new Swiper('.works__slider', {
+    navigation: {
+        nextEl: ".icon-right-open",
+        prevEl: ".icon-left-open",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    sliderPerView: 1,
+    loop: true,
+    breakpoints: {
+    // when window width is 
+    1200: {
+        slidesPerView: 3,
+        spaceBetween: 5
+    },
+    // when window width is
+    1920: {
+        spaceBetween: 35
+    },
+    },
+    
+    modules: [Navigation, Pagination],
+});
+} catch(e) {}
+
